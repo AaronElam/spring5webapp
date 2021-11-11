1051fbfc2a528f37eeac2bd27ad1ff14d1413c0b
AaronElam <speztra97@gmail.com>
2021-11-10 20:00:15 -0600

    JPA entities done

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
